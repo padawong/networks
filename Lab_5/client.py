27cbbcc1b883ec1686892d1e8cb7aaee03e2daee
padawong <erin.b.wong@gmail.com>
2019-12-06 02:52:56 -0800

    Working Lab 5

--- Lab_5/client.py
+++ Lab_5/client.py
@@ -1,8 +1,12 @@
 import socket   #for sockets
 import sys      #for exit
 from check import ip_checksum
+import threading #for timeout
 
-# Create dgram udp socket
+timeout = False
+msg = ''
+n = ''
+
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # if socket functions fail, catch exception
@@ -14,41 +18,73 @@ except socket.error, msg:
 host = 'localhost'
 port = 8888
 
+def pkt_timer():
+    timeout = True
+    print('\nTimeout detected')
+    print('Resending timed-out packet ' + str(n) + '\n')
+    s.sendto(msg, (host, port))
+timer = threading.Timer(3.0, pkt_timer)
+
+# Create dgram udp socket
+
+# Client is sender
+
 # Packet # counter
 n = 0
-while(1):
+
+# Set up initial packet
+# Scenario 1: Packets 0-2
+print('\nSending 3 packets normally:\n')
+msg = 'packet ' + str(n)
+check_sum = ip_checksum(msg)
+msg += ';' + str(ord(check_sum[0]) + ord(check_sum[1]))
+s.sendto(msg, (host, port))
+
+while(n < 9):
     # msg = raw_input('Enter message to send: ')
-    msg = 'packet' + n
+    # print('check_sum ' + str(ord(check_sum[0])) + '; pt 2: ' + str(ord(check_sum[1])))
 
     try:
-        # Set the whole string
-        s.sendto(msg, (host, port))
+        if n == 6:
+            timer.start()
 
         # Receive data from client (data, addr)
         d = s.recvfrom(1024)
         reply = d[0]
         addr = d[1]
 
-        # Send first 3 packets normally
-        if reply == 'ack packet' + n:
-            print('ack packet' + n + 'received')
-            n += 1
-
-        # Send 3 packets where 2nd packet is corrupted
         # If data corrupted, resend
-        elif ip_checksum(d) != 0:
+        if 'CORRUPTED: ' in reply:
+            print('Resending corrupted packet ' + str(n))
             s.sendto(msg, (host, port))
 
         # Send 3 packets where 2nd packet times out (delay ack)
         # If packet times out, resend
         elif timeout:
+            print('Resending timed-out packet ' + str(n))
             s.sendto(msg, (host, port))
 
+        if reply == 'ack packet ' + str(n):
+        #else:
+            print('ack packet ' + str(n) + ' received')
             
+            # Send next packet
+            if n >= 8:
+                break
+            n += 1
+            # Scenario 2: Packets 3-5
+            if n == 3:
+                print('\nSending 3 packets where second packet (pkt 4) has invalid checksum:\n')
+            # Scenario 3: Packets 6-8
+            if n == 6:
+                print('\nSending 3 packets where second packet (pkt 7) times out:\n')
+            msg = 'packet ' + str(n)
+            check_sum = ip_checksum(msg)
+            msg += ';' + str(ord(check_sum[0]) + ord(check_sum[1]))
+            s.sendto(msg, (host, port))
 
         # print('Server reply: ' + reply)
     
     except socket.error, msg:
         print('Error Code: ' + str(msg[0]) + 'Message' + msg[1])
         sys.exit()
-    n += 1
--- Lab_5/server.py
+++ Lab_5/server.py
@@ -1,8 +1,22 @@
 import socket
 import sys
+from check import ip_checksum
+# import threading
+import time
 
 HOST = ''
 PORT = 8888
+
+reply = ''
+addr = ''
+
+"""
+def pkt_timer():
+    timeout = True
+    print('\nSending timed-out ' + reply)
+    s.sendto(reply, addr)
+timer = threading.Timer(3.1, pkt_timer)
+"""
 
 # Datagram (udp) socket
 try:
@@ -21,6 +35,14 @@ except socket.error , msg:
 
 print('Socket bind complete')
 
+# Server is receiver
+corrupted_sent = False
+timeout = False
+timeout_sent = False
+
+prev = ''
+prev_ack = ''
+
 # Now keep talking with the client
 while 1:
     d = s.recvfrom(1024)
@@ -29,21 +51,53 @@ while 1:
 
     if not data:
         break
-
-
-    # Receive 3 packets where 2nd packet is corrupted
-    if data == 'packet 4':
-        reply = 
     
-    # Receive 3 packets where 2nd packet times out (delay ack)
-    elif data == 'packet 7':
+    # Split input into data and checksum
+    data = data.split(';', 1)
+    checksum_exp = int(data[1])
+    data = data[0]
 
-    # Receive all other packets normally
-    else:
-        reply = 'ack ' + data
-    # reply = 'OK...' + data
-
-    s.sendto(reply, addr)
     print('Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip())
 
-s.close()
+    #print('checksum_exp = ' + str(checksum_exp))
+    #print('checksum_exp type = ' + str(type(checksum_exp)))
+    #print('data = ' + data)
+
+    checksum_actual = ip_checksum(data)
+    checksum_actual = ord(checksum_actual[0]) + ord(checksum_actual[1])
+    #print('checksum_actual = ' + str(checksum_actual))
+    #print('checksum_actual type = ' + str(type(checksum_actual)))
+
+    if not corrupted_sent and data == 'packet 4':
+        checksum_actual += 1
+        corrupted_sent = True
+
+    if checksum_exp != checksum_actual:
+        reply = 'CORRUPTED: ' + data
+        print(reply)
+        print('\tchecksum_exp = ' + str(checksum_exp))
+        print('\tchecksum_actual = ' + str(checksum_actual))
+    # Receive all other packets normally
+
+    if not timeout and data == 'packet 7':
+        print('Receiver forcing timeout for: ' + data)
+        time.sleep(3.1)
+        timeout = True
+        """
+        if not timer.isAlive():
+            timer.start()
+        timeout_sent = True
+        reply = 'ack ' + data
+        """
+
+    # Duplicate packet received; resend ack
+    if data == prev and timeout:
+        reply = prev_ack
+
+    else:
+        reply = 'ack ' + data
+        prev_ack = reply
+
+    # print('REPLY = ' + reply)
+    s.sendto(reply, addr)
+    prev = data
